48d027a41e9c43ba04e09d10ad1b8fd19c699609
김영석 <shinerdes@naver.com>
2018-09-05 02:15:34 +0900

    Collection views

---
+++ coder-swag/View/ProductCell.swift
@@ -0,0 +1,15 @@
+//
+//  ProductCell.swift
+//  coder-swag
+//
+//  Created by 김영석 on 2018. 9. 5..
+//  Copyright © 2018년 김영석. All rights reserved.
+//
+
+import UIKit
+
+class ProductCell: UICollectionViewCell {
+    @IBOutlet weak var productImage: UIImageView!
+    @IBOutlet weak var productTitle: UILabel!
+    @IBOutlet weak var productPrice: UILabel!
+}
